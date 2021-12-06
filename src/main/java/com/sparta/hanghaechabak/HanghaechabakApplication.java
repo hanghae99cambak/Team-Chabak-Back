9b7a6d8960836f55bc809837227288dcb67d5f87
naocya@naver.com <naocya@naver.com>
2021-12-06 14:03:55 +0900

    first commit

---
+++ src/main/java/com/sparta/hanghaechabak/HanghaechabakApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hanghaechabak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaechabakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaechabakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/hanghaechabak/HanghaechabakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.hanghaechabak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaechabakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
