e9b548d3bcfb81aaa5ca1cb28d2cb45740531d8c
Zpoxic <jjm03076@naver.com>
2021-12-07 03:45:12 +0900

    Kakao 로그인 기능 API에 따라 변경 필요

--- src/main/java/com/sparta/hanghaechabak/model/User.java
+++ src/main/java/com/sparta/hanghaechabak/model/User.java
@@ -5,6 +5,7 @@ import lombok.*;
 import javax.persistence.*;
 
 @Builder
+@Setter
 @Getter // get 함수를 일괄적으로 만들어줍니다.
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자를 만들어줍니다.
@@ -24,6 +25,26 @@ public class User extends Timestamped {
     @Column(nullable = false)
     private String email;
 
-    private String kakaoId;
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
 
+    @Column(unique = true)
+    private Long kakaoId;
+
+    public User(String nickname, String password, String email, UserRoleEnum role) {
+        this.nickname = nickname;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.kakaoId = null;
+    }
+
+    public User(String nickname, String password, String email, UserRoleEnum role, Long kakaoId) {
+        this.nickname = nickname;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.kakaoId = kakaoId;
+    }
 }
--- src/main/java/com/sparta/hanghaechabak/repository/UserRepository.java
+++ src/main/java/com/sparta/hanghaechabak/repository/UserRepository.java
@@ -2,6 +2,13 @@ package com.sparta.hanghaechabak.repository;
 
 import com.sparta.hanghaechabak.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByNickname(String nickname);
+    Optional<User> findByKakaoId(Long kakaoId);
+    Optional<User> findByEmail(String email);
 }
