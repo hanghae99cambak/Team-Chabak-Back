86443aab12f72d887dffa2fc5053611fbf145bad
woojin126 <woojin126@naver.com>
2021-12-07 00:20:37 +0900

    BoardController

--- src/main/java/com/sparta/hanghaechabak/controller/BoardController.java
+++ src/main/java/com/sparta/hanghaechabak/controller/BoardController.java
@@ -4,8 +4,9 @@ import com.sparta.hanghaechabak.service.BoardService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+
 
 @Api(tags = {"Board"})
 @RestController
@@ -14,34 +15,30 @@ public class BoardController {
 
     private BoardService boardService;
 
-    @ApiOperation(value = "게시글 등록")
+/*    @ApiOperation(value = "게시글 등록")
     @PostMapping("/api/board")
-    public void boardUpload(
-    ) {
-    }
+    public void boardUpload(@RequestBody @Valid BoardRequestDto boardRequestDto, *//*,@AuthenticationPrincipal CustomUserDetail userDetail*//*) {
+        boardService.savePost(boardRequestDto, userDetail.getUser());
+    }*/
 
     @ApiOperation(value = "게시글 수정")
     @PutMapping("/api/board/detail/{id}")
-    public void boardUpdate(
-    ) {
+    public void boardUpdate(@PathVariable Long id) {
     }
 
     @ApiOperation(value = "게시글 조회")
     @GetMapping("/api/board")
-    public void boardGetList(
-    ) {
+    public void boardGetList() {
     }
 
 
     @ApiOperation(value = "게시글 상세조회")
     @GetMapping("/api/board/detail/{id}")
-    public void boardGetDetail(
-    ) {
+    public void boardGetDetail() {
     }
 
     @ApiOperation(value = "게시글 삭제")
-    @DeleteMapping ("/api/board/detail/{id}")
-    public void boardDelete(
-    ) {
+    @DeleteMapping("/api/board/detail/{id}")
+    public void boardDelete() {
     }
 }
