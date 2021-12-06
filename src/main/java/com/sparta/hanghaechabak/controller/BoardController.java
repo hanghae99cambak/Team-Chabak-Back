970318bd83bedf58774e64e0436f2046bec29d28
naocya@naver.com <naocya@naver.com>
2021-12-06 17:54:26 +0900

    security disable

--- src/main/java/com/sparta/hanghaechabak/controller/BoardController.java
+++ src/main/java/com/sparta/hanghaechabak/controller/BoardController.java
@@ -4,7 +4,6 @@ import com.sparta.hanghaechabak.service.BoardService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Api(tags = {"Board"})
