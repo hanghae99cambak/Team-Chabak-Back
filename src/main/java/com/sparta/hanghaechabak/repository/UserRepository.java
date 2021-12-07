784717158771cdf09834365596f8bad26eb43821
naocya@naver.com <naocya@naver.com>
2021-12-08 02:12:45 +0900

    초기화

--- src/main/java/com/sparta/hanghaechabak/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.hanghaechabak.repository;
-
-import com.sparta.hanghaechabak.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
